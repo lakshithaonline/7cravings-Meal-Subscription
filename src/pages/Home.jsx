f632bdfca897bcfc7d794b56ced442a39d0b50e8
Asirimath Vimukthi <asirimath20@gmail.com>
2024-07-12 01:10:42 +0530

    #3 Section one 1&2 done.(important tag removed  from index)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,73 @@ import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import homeone from "../assets/images/homeOneImg.jpg";
 import "../styles/hero-section.css";
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const itemData = [
+    {
+        img: 'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Breakfast',
+    },
+    {
+        img: 'https://images.pexels.com/photos/1410235/pexels-photo-1410235.jpeg',
+        title: 'Burger',
+    },
+    {
+        img: 'https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=600',
+        title: 'Camera',
+    },
+    {
+        img: 'https://images.pexels.com/photos/2955819/pexels-photo-2955819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Coffee',
+    },
+    {
+        img: 'https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Hats',
+    },
+    {
+        img: 'https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Honey',
+    },
+    {
+        img: 'https://images.pexels.com/photos/4109111/pexels-photo-4109111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Basketball',
+    },
+    {
+        img: 'https://images.pexels.com/photos/2365945/pexels-photo-2365945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Fern',
+    },
+    {
+        img: 'https://images.pexels.com/photos/2059150/pexels-photo-2059150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Mushrooms',
+    },
+    {
+        img: 'https://images.pexels.com/photos/1640771/pexels-photo-1640771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        title: 'Tomato basil',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+        title: 'Sea star',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+        title: 'Bike',
+    },
+];
 
 const Home = () => {
     return (
         <Helmet title="Home">
             <div className="single-scroll-page">
                 {/* Section 1 */}
-                <section className="section section-1 vh-100">
+                <section className="section section-1 vh-100" style={{
+                    backgroundImage: 'url("https://images.pexels.com/photos/1600732/pexels-photo-1600732.jpeg")',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}>
                     <Container className="h-100">
-                        <Row className="h-100">
+                        <Row className="h-100" >
                             <Col lg="8" md="6" className="h-100">
                                 <div className="hero__content">
                                     <h5 className="mb-3">Weekly Meal Kits</h5>
@@ -62,20 +120,17 @@ const Home = () => {
                                 </div>
                             </Col>
                             <Col lg="4" md="6" className="h-100">
-
                                 <div className="rectangle__one">
                                     <div className="rec__one_text">
-                                    <h1 className="rec_one_t"> Make your </h1>
-                                    <h1 className="rec_one_t"> own </h1>
-                                    <h1 className="rec_one_t"> Meal kit </h1>
-                                    <h1 className="rec_one_t"> Today </h1>
-
+                                        <h1 className="rec_one_t"> Make your </h1>
+                                        <h1 className="rec_one_t"> own </h1>
+                                        <h1 className="rec_one_t"> Meal kit </h1>
+                                        <h1 className="rec_one_t"> Today </h1>
                                         <div className="reg_now_sec">
                                             <Link to="/menu" className="reg_now_btn">
                                                 Start Cooking
                                             </Link>
                                         </div>
-
                                     </div>
                                     <div>
                                         <p>
@@ -89,15 +144,42 @@ const Home = () => {
                 </section>
 
                 {/* Section 2 */}
-                <section className="section section-2 vh-100">
+                <section className="section section-2 vh-200">
                     <Container className="h-100">
                         <Row className="h-100">
                             <Col lg="4" md="6" className="h-100">
-                                <h2>Section 2</h2>
-                                <p>Add your content here for Section 2.</p>
+                                <div style={{ paddingTop:"10px"}}>
+                                    <h2>50+ Recipes Every Week! </h2>
+
+                                    <h5>Our expanded menu gives you more choice and even more price flexibility. So you
+                                        can easily cook your favorite meals—without breaking a sweat.</h5>
+                                    <br/>
+                                    <h2>Ease Your Stress On Your Food </h2>
+
+                                    <h5>More time-More work </h5>
+                                </div>
+                                <div className="reg_now_sec">
+                                    <Link to="/menu" className="reg_now_btn">
+                                        Start Cooking
+                                    </Link>
+                                </div>
                             </Col>
-                            <Col lg="6" md="6" className="h-100">
-                                {/* Add relevant content for Section 2 */}
+                            <Col lg="8" md="6" className="h-500">
+
+                                <Box sx={{width: '100%', height: 450, overflowY: 'scroll'}}>
+                                    <ImageList variant="masonry" cols={3} gap={8}>
+                                    {itemData.map((item) => (
+                                            <ImageListItem key={item.img}>
+                                                <img
+                                                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                                                    alt={item.title}
+                                                    loading="lazy"
+                                                />
+                                            </ImageListItem>
+                                        ))}
+                                    </ImageList>
+                                </Box>
                             </Col>
                         </Row>
                     </Container>
@@ -107,7 +189,6 @@ const Home = () => {
                 <section className="section section-3 vh-100">
                     <Container className="h-100">
                         <Row className="h-100">
-
                             <Col lg="8" md="6" className="h-100">
                                 <h2>Section 3</h2>
                                 <p>Add your content here for Section 3.</p>

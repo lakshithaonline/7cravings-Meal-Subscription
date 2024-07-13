8230cbffce7b10545c3ed704b56247bcb4386cd1
Lakshitha Geethmal <87240720+lakshithaonline@users.noreply.github.com>
2024-07-13 21:33:16 +0530

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+//replace the Firebase config 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAr2sTDhwIVvndzmY-g7jr-le3hw1WWo_Q",
-    authDomain: "cravings-3abb3.firebaseapp.com",
-    projectId: "cravings-3abb3",
-    storageBucket: "cravings-3abb3.appspot.com",
-    messagingSenderId: "987816101665",
-    appId: "1:987816101665:web:98af517a0dfdad3c332060"
+    apiKey: "XXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 
 // Initialize Firebase
